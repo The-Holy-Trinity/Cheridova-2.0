697cd704383216b8249b061be0d9348c72e40847
annabellachan <annabellac24@gmail.com>
2023-01-24 22:18:20 -0800

    fixed the footer size, index styling, and made some changes to seeds file

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-beige fixed bottom-0 left-0 z-20 w-full p-4 flex md:p-4">
+    <footer className="bg-beige fixed bottom-0 left-0 z-20 w-full px-2 py-1 font-garamond">
       <div className="container text-center">
-        <p class="text-base text-center">
+        <p className="text-base text-center">
           © 2023 Chéridova By: Annabella, Charlie, and Shannon. All Rights
           Reserved.
         </p>
