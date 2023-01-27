6577abf18f07be618c31dae8025c3cde755e3ea5
annabellachan <annabellac24@gmail.com>
2023-01-27 11:43:37 -0800

    category and index styling completed

--- app/javascript/components/pages/Category.js
+++ app/javascript/components/pages/Category.js
@@ -1,51 +1,114 @@
 import React from "react";
-// import "./Category.css";
 import { NavLink } from "react-router-dom";
+import { Card, CardBody, CardSubtitle } from "reactstrap";
+import casual1 from "../../../assets/photos/casual1.png";
+import fm2 from "../../../assets/photos/fm2.png";
+import party from "../../../assets/photos/party.png";
+import office from "../../../assets/photos/office.png";
+
 const Category = (props) => {
   return (
-    <div
-      className="blue"
-      style={{ width: "100%", display: "flex", flexDirection: "column" }}
-    >
-      <h1
-        style={{
-          fontSize: "40px",
-          textAlign: "center",
-        }}
-      >
-        Categories
-      </h1>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-around",
-        }}
-      >
-        <NavLink to="/OutfitsOffice" className="nav-link">
-          <button>Office Attire</button>
-        </NavLink>
-
-        <NavLink to="/OutfitsCasual" className="nav-link">
-          <button>Casual</button>
-        </NavLink>
-
-        <NavLink to="/OutfitsFormal" className="nav-link">
-          <button>Formal</button>
-        </NavLink>
-
-        <NavLink to="/OutfitsDate" className="nav-link">
-          <button>Date Night</button>
+    <div className="container justify-items-center text-center space-y-6">
+      <div className="space-y-4">
+        <h1 className="text-4xl font-garamond text-center space-y-4">
+          Categories
+        </h1>
+        <NavLink to="/Outfits" className="nav-link">
+          <button className="bg-coral hover:bg-gray text-white font-bold font-garamond py-2 px-2 border-b-2 border-gray hover:border-green rounded">
+            All Outfits
+          </button>
         </NavLink>
       </div>
+      <div className="lg:grid lg:grid-cols-4 space-y-8 lg:gap-3 content-start">
+        {/* office attire */}
+        <div className="text-center space-y-4 pt-4 mt-2">
+          <Card
+            style={{
+              width: "14rem",
+            }}
+          >
+            <CardBody className="bg-lightgray text-white font-garamond">
+              <CardSubtitle className="mb-2" tag="h6">
+                <NavLink to="/OutfitsOffice" className="nav-link">
+                  <img
+                    alt="office"
+                    className="object-cover h-48 w-96"
+                    src={office}
+                  />
+                  <button className="bg-green hover:bg-gray text-white font-bold font-garamond py-1 px-1 border-b-2 border-gray hover:border-green rounded">
+                    Office Attire
+                  </button>
+                </NavLink>
+              </CardSubtitle>
+            </CardBody>
+          </Card>
+        </div>
 
-      <div
-        style={{
-          textAlign: "center",
-        }}
-      >
-        <NavLink to="/Outfits" className="nav-link">
-          <button>All Outfits</button>
-        </NavLink>
+        {/* casual*/}
+        <div className="text-center space-y-4">
+          <Card
+            style={{
+              width: "14rem",
+            }}
+          >
+            <CardBody className="bg-lightgray text-white font-garamond">
+              <NavLink to="/OutfitsCasual" className="nav-link">
+                <img
+                  alt="casual1"
+                  className="object-cover h-48 w-96"
+                  src={casual1}
+                />
+                <button className="bg-green hover:bg-gray text-white font-bold font-garamond py-1 px-1 border-b-2 border-gray hover:border-green rounded">
+                  Casual
+                </button>
+              </NavLink>
+            </CardBody>
+          </Card>
+        </div>
+
+        {/* formal */}
+        <div className="text-center space-y-4">
+          <Card
+            style={{
+              width: "14rem",
+            }}
+          >
+            <CardBody className="bg-lightgray text-white font-garamond">
+              <NavLink to="/OutfitsFormal" className="nav-link">
+                <img
+                  alt="formal"
+                  className="object-cover h-48 w-96"
+                  src={fm2}
+                />
+                <button className="bg-green hover:bg-gray text-white font-bold font-garamond py-1 px-1 border-b-2 border-gray hover:border-green rounded">
+                  Formal
+                </button>
+              </NavLink>
+            </CardBody>
+          </Card>
+        </div>
+
+        {/* date night */}
+        <div className="text-center space-y-4">
+          <Card
+            style={{
+              width: "14rem",
+            }}
+          >
+            <CardBody className="bg-lightgray text-white font-garamond">
+              <NavLink to="/OutfitsDate" className="nav-link">
+                <img
+                  alt="party"
+                  className="object-cover h-48 w-96"
+                  src={party}
+                />
+                <button className="bg-green hover:bg-gray text-white font-bold font-garamond py-1 px-1 border-b-2 border-gray hover:border-green rounded">
+                  Date Night
+                </button>
+              </NavLink>
+            </CardBody>
+          </Card>
+        </div>
       </div>
     </div>
   );
