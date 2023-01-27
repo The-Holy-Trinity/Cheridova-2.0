632b3ea756b7244b3bcdc5d8b6803f817210a576
LEARN Academy <learnacademy@LEARNs-Air.attlocal.net>
2023-01-27 10:41:51 -0800

    changes to about us

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
--- app/javascript/components/pages/Aboutus.js
+++ app/javascript/components/pages/Aboutus.js
@@ -2,7 +2,54 @@ import React from "react";
 const Aboutus = (props) => {
   return (
     <>
+<<<<<<< Updated upstream
       <h1 style={{ textAlign: "center" }}>About us</h1>
+=======
+      <h1
+        style={{
+          textAlign: 'center',
+          fontWeight: 'bold',
+          fontSize: '50px',
+          fontStyle: 'Helvetica-Oblique',
+          color: 'teal',
+        }}
+      >
+        Meet the Developers
+      </h1>
+     
+      <img src={Charlie} width="200" height="200" />
+      <h3
+        style={{
+          textAlign: 'left',
+          color: 'teal',
+          wordWrap: 'normal',
+          wordWrap: 'break-word',
+        }}
+      >
+        Charlie Si, an avid gamer with a great eye for fashion if you're a
+        sweatshirt and jeans type of guy
+      </h3>
+      <br />
+      <img src={Bella} align="right" width="200" height="200" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <h3 style={{ textAlign: 'right', color: 'teal' }}>
+        Annabella Chan, a San Diego native, with a natural California girl style
+      </h3>
+      <img src={Shannon} width="200" height="200" />
+      <h3 style={{ textAlign: 'left', color: 'teal' }}>
+        Shannon Young, a self proclaimed fashionista on a budget who can always
+        find the most stylish things at prices that won't break the bank
+      </h3>
+      <br />
+>>>>>>> Stashed changes
     </>
   );
 };
