bbb85a29a86c275cb49d093d19f18957868de669
annabellachan <annabellac24@gmail.com>
2023-01-27 16:20:45 -0800

    protected index css styling completed

--- app/javascript/components/pages/OutfitProtectedIndex.js
+++ app/javascript/components/pages/OutfitProtectedIndex.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
+import { Card, CardBody, CardTitle, CardSubtitle, button } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
 const OutfitProtectedIndex = ({
@@ -13,42 +13,60 @@ const OutfitProtectedIndex = ({
   });
   return (
     <>
-      <h2>My Fashionable Tastes</h2>
-      {current_user_outfits?.map((outfit, index) => {
-        return (
-          <Card
-            style={{
-              width: "14rem",
-            }}
-            key={index}
-          >
-            <img alt={`profile of outfits ${outfit.name}`} src={outfit.image} />
-            <CardBody>
-              <CardSubtitle className="mb-2 text-muted" tag="h6">
-                Name: {outfit.name}
-              </CardSubtitle>
-              <CardSubtitle className="mb-2 text-muted" tag="h6">
-                Style: {outfit.style}
-              </CardSubtitle>
-              <CardSubtitle className="mb-2 text-muted" tag="h6">
-                Gender: {outfit.gender}
-              </CardSubtitle>
-              <NavLink to={`/OutfitsShow/${outfit.id}`}>
-                <Button>See More Details</Button>
-              </NavLink>
-              <NavLink to="/Outfits">
-                <Button onClick={() => deleteOutfit(`${outfit.id}`)}>Delete this outfit</Button>
-                <Button onClick={() => deleteOutfit(`${outfit.id}`)}>
-                  Delete this outfit
-                </Button>
-              </NavLink>
-              <NavLink to={`/OutfitEdit/${outfit.id}`}>
-                <Button>Edit this outfit</Button>
-              </NavLink>
-            </CardBody>
-          </Card>
-        );
-      })}
+      <div>
+        <h2 className="font-garamond text-center pt-2">My Fashionable Taste</h2>
+      </div>
+      <div className="container mx-auto space-y-2 lg:space-y-0 lg:gap-2 items-center">
+        <div className="lg:grid lg:grid-cols-3 justify-items-center gap-8">
+          {current_user_outfits?.map((outfit, index) => {
+            return (
+              <Card
+                style={{
+                  width: "14rem",
+                }}
+                key={index}
+              >
+                <img
+                  alt={`profile of outfits ${outfit.name}`}
+                  src={outfit.image}
+                  className="object-cover h-48 w-96"
+                />
+                <CardBody>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    Name: {outfit.name}
+                  </CardSubtitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    Style: {outfit.style}
+                  </CardSubtitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    Gender: {outfit.gender}
+                  </CardSubtitle>
+                  <div className="flex flex-col space-y-2">
+                    <NavLink to={`/OutfitsShow/${outfit.id}`}>
+                      <button className="bg-green hover:bg-gray text-white font-bold font-garamond py-1 px-1 border-b-2 border-gray hover:border-green rounded">
+                        See More Details
+                      </button>
+                    </NavLink>
+                    <NavLink to="/Outfits">
+                      <button
+                        className="bg-green hover:bg-gray text-white font-bold font-garamond py-1 px-1 border-b-2 border-gray hover:border-green rounded"
+                        onClick={() => deleteOutfit(`${outfit.id}`)}
+                      >
+                        Delete this outfit
+                      </button>
+                    </NavLink>
+                    <NavLink to={`/OutfitEdit/${outfit.id}`}>
+                      <button className="bg-green hover:bg-gray text-white font-bold font-garamond py-1 px-1 border-b-2 border-gray hover:border-green rounded">
+                        Edit this outfit
+                      </button>
+                    </NavLink>
+                  </div>
+                </CardBody>
+              </Card>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
